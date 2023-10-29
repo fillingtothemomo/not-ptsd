4e4e3940474153eb2ebf1cae6b789e7849a7f745
Aayushmaan <goforaayush@gmail.com>
2023-10-29 07:23:02 +0530

    ptch: adds frontend skeleton

--- api/mangas/models.py
+++ api/mangas/models.py
@@ -8,3 +8,4 @@ class Manga(models.Model):
     description = models.TextField(null=True)
     cover_image = models.ImageField(upload_to='manga_covers/', null=True)
     manga_file = models.FileField(upload_to=manga_pdf_upload_path, null=True)
+    base64=models.TextField(null=True)
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,26 @@
-import './App.css';
-
+import "./App.css";
+import React, { useEffect, lazy } from "react";
+import router from ""
+import list from "../../api/media/manga_covers/default-avatar-profile-icon-of-social-media-user-vector_D4eX6c4.jpg";
+// const importtt = lazy(() =>
+//   import(
+//     "../../api/media/manga_covers/default-avatar-profile-icon-of-social-media-user-vector_D4eX6c4.jpg"
+//   )
+// );
 function App() {
+  // importtt();
+  const coverImage = "xyz,abc,erjk,wd,jnlw"
+  const coverImageArray = coverImage.split(",");
+  console.log(coverImageArray);
+  let hi = 5;
+  console.log(hi);
   return (
-  <div></div>
+    <main>
+      <h1>Manga Maestro</h1>
+      {coverImageArray.map((coverImage) => {
+        return <img src={coverImage} ></img>
+      })}
+    </main>
   );
 }
 
